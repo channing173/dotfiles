e4b5439ad6e95ea38500e496684b543e9d4a2162
channing173 <cgwthree@gmail.com>
2019-02-15 01:19:24 -0500

    Changed to intel video settings

--- Razer Blade/home.sh
+++ Razer Blade/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --mode 2560x1080 --rate 75 --pos 0x0 --rotate normal --output eDP-1 --off
+xrandr --output HDMI1 --off --output DP1 --mode 2560x1080 --rate 75 --pos 0x0 --rotate normal --output eDP1 --off --output VIRTUAL1 --off
